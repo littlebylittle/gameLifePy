879bc4ada8ffe647c7307e24fb9c1407558ad755
Igor Inkognito <iggymas@gmail.com>
2013-03-07 23:04:26 +0400

    little fix, removed stuped comment

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: UTF-8 -*-
-#  * Date: 3/7/13
-#  * Time: 6:18 PM
-#  =============================
-from __future__ import print_function
 from lifeengine.game import *
 
 
